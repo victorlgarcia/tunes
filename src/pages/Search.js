58203e02050203845e8a58bf9033f52444749dbf
Victor Lima Garcia <victorlimagarcia@gmail.com>
2022-07-05 20:44:37 -0300

    Requisito 5 completo

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,16 +1,57 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      btnDisabled: true,
+    };
+  }
+
+  handleValue = (event) => {
+    this.setState({
+      input: event.target.value,
+    }, this.handleCondition);
+  }
+
+  handleCondition = () => {
+    const { input } = this.state;
+    const size = 2;
+    if (input.length >= size) {
+      this.setState({
+        btnDisabled: false,
+      });
+    } else {
+      this.setState({
+        btnDisabled: true,
+      });
+    }
+  }
+
   render() {
+    const { btnDisabled } = this.state;
     return (
       <div data-testid="page-search">
 
         <Header />
-        <Link to="/"> Home </Link>
+        <form>
+          <input
+            data-testid="search-artist-input"
+            onChange={ this.handleValue }
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ btnDisabled }
+          >
+            Pesquisar
 
-        <p>Conteúdo da Search</p>
+          </button>
+        </form>
+
       </div>
     );
   }
